b549ffec61e68f8dc23df37baa1ea14ccd7f5865
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-21 19:39:53 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
