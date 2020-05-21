b4d75dd918121d6cdac54258c204bd3d3a4c132f
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-21 20:21:49 -0300

    Update index.js

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
