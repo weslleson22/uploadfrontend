1e1ef8702810902d4d99a42785903a3840986be9
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-21 19:23:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './global.css';
 
-//import Routes from './routes';
+import Routes from './routes';
 
     function App() {
       return (
-          //<Routes />  
-          <h1>Hello wesleson</h1>
+          <Routes />  
+         
         
       );
     }
