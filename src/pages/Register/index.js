924116ba67b7816114e0feedd334484acd51268d
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-21 20:25:51 -0300

    Update index.js

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
 import api from '../../services/api'
