864a32f30e5fcee697a178742c7396fdfd4916dd
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-21 21:43:47 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import './global.css';
+
+import Routes from './routes';
+
+    function App() {
+      return (
+          <Routes />  
+        
+      );
+    }
+
+export default App;
